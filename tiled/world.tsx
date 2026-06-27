f46eb0a24a48f85ac16614f25d2a6709a23beb2a
Evolyonne <evolyonne012@gmail.com>
2026-06-27 09:57:02 +0200

    add asset

---
+++ tiled/world.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="world" tilewidth="64" tileheight="64" tilecount="2160" columns="54">
+ <image source="../../kenney_top-down-shooter/Tilesheet/tilesheet_complete_2X.png" width="3456" height="2560"/>
+</tileset>
